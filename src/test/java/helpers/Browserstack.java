bfc676d2f42a53194abbb738f5c1b797bb7713cd
Julia Menschikova <yu.menschikowa2015@yandex.ru>
2023-12-24 20:48:24 +0300

    add utf-8

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -8,13 +8,16 @@ import static io.restassured.RestAssured.given;
 
 
 public class Browserstack {
+
+    // curl -u "qaguru_ti9G5S:5yrxu4nFTKkRExUAhqxh" -X GET "https://api.browserstack.com/app-automate/sessions/0359d759d2aaa4f46401dac46bd281b6d9b24943.json"
+    // automation_session.video_url
     static WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
     public static String videoUrl(String sessionId) {
 
         String url = String.format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
-                .auth().basic(config.getUserName(), config.getAccessKey())
+                .auth().basic(config.getUserName(), config.getUrl())
                 .get(url)
                 .then()
                 .log().status()
